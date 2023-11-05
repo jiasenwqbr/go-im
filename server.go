9f8128a4ae9aca444c90149daa25f4187455792f
jiasenwqbr <jasonjiasen@gmail.com>
2023-11-05 16:50:30 +0800

    add readme.md

--- server.go
+++ server.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	// 当前连接的业务
+	fmt.Println("连接建立成功")
+}
+
+//启动服务器接口
+func (this *Server) start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net Listent err", err)
+		return
+	}
+	// close listen port
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Listener accept err:", err)
+		}
+		// do handler
+		go this.Handler(conn)
+	}
+}
