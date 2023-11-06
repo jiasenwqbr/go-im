35141dbd6513082071e4faf337d830a846a0c9d0
jiasenwqbr <jasonjiasen@gmail.com>
2023-11-06 11:20:57 +0800

    Overtime strong kick function

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -51,6 +52,8 @@ func (this *Server) Handler(conn net.Conn) {
 	user := NewUser(conn, this)
 
 	user.Online()
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
 	// 接受客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -68,14 +71,34 @@ func (this *Server) Handler(conn net.Conn) {
 			msg := string(buf[:n-1])
 			// 用户针对msg进行消息处理
 			user.DoMessage(msg)
+			//用户的任意消息，代表当前用户是一个活跃的
+			isLive <- true
 		}
 	}()
 	// 当前用户阻塞
-	select {}
+
+	for {
+		select {
+		case <-isLive:
+			fmt.Println("isLive")
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，为了激活select，更新下面的定时器
+		case <-time.After(time.Second * 100):
+			//已经超时
+			//将当前的user强制关闭
+			user.SendMsg("你被踢了")
+			// 销毁用的资源
+			close(user.C)
+			// 关闭连接
+			conn.Close()
+			//退出当前的handler
+			return
+		}
+	}
 
 }
 
-//启动服务器接口 Start Server Interface
+// Start 启动服务器接口 Start Server Interface
 func (this *Server) Start() {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
